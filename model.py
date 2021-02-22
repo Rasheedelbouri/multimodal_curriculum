4dfa5772a102eeb12683e7e058fbac7056d1990b
Rasheedelbouri <rasheed.el-bouri@eng.ox.ac.uk>
2021-02-22 12:01:09 +0000

    Adding training

---
+++ model.py
@@ -0,0 +1,19 @@
+import torch
+
+
+
+class Feedforward(torch.nn.Module):
+        def __init__(self, input_size, hidden_size):
+            super(Feedforward, self).__init__()
+            self.input_size = input_size
+            self.hidden_size  = hidden_size
+            self.fc1 = torch.nn.Linear(self.input_size, self.hidden_size)
+            self.relu = torch.nn.ReLU()
+            self.fc2 = torch.nn.Linear(self.hidden_size, 1)
+            self.sigmoid = torch.nn.Softmax()
+        def forward(self, x):
+            hidden = self.fc1(x)
+            relu = self.relu(hidden)
+            output = self.fc2(relu)
+            output = self.sigmoid(output)
+            return output
